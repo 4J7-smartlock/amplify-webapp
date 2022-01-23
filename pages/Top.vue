9b7c617d7c1755de457991f91e8effdae4566ae6
Satou73 <ja3173ila@gmail.com>
2022-01-23 11:50:24 +0900

    完成

---
+++ pages/Top.vue
@@ -0,0 +1,132 @@
+<template>
+  <v-card
+    max-width="400"
+    class="mx-auto">
+    <v-container fluid > 
+      <v-row dense>
+        
+        <v-col cols="12">
+          <v-card
+            color="#406882"
+            dark>
+              <v-card-title>
+                My 施錠確認
+              </v-card-title>
+              <v-card-text>
+              </v-card-text>
+            </v-card>
+        </v-col>
+
+        <v-col cols="6">
+       <div v-if="items"> 
+        <div v-if= " items[0]['lock_status'] == 'open'">
+          <v-card
+           color="#00FF7F"
+           height="160"
+           light> 
+           <v-card-title>武道館 </v-card-title>
+           <v-card-text>
+             <v-icon mx-auto size="50">mdi-lock-open</v-icon>
+           </v-card-text>
+          </v-card>
+        </div>
+        <div v-else>
+          <v-card
+           color="#ff4500"
+           height="160"
+           dark> 
+           <v-card-title>武道館 </v-card-title>
+           <v-card-text>
+             <v-icon mx-auto size="50">mdi-lock</v-icon>
+           </v-card-text>
+          </v-card>
+        </div>
+       </div>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card
+            color="#406882"
+            dark>
+              <v-card-title>
+                ご利用にあたり
+              </v-card-title>
+              <v-card-text>
+              </v-card-text>
+            </v-card>
+        </v-col>
+
+        <v-col cols="6">
+          <v-card
+            color="#ff4500"
+          　dark>
+          <v-card-title>武道館</v-card-title>
+          <v-card-text>
+           
+             <v-icon center size="50">mdi-lock</v-icon>
+            ※施錠されている。
+          </v-card-text>
+          </v-card>
+        </v-col>
+        
+        <v-col cols="6">
+          <v-card
+           color="#00FF7F"
+           light> 
+           <v-card-title>武道館 </v-card-title>
+           <v-card-text>
+             <v-icon mx-auto size="50">mdi-lock-open</v-icon>
+              ※開錠されている。
+           </v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card
+            color="#1A374D"
+            dark>
+              <v-card-title>
+                補足
+              </v-card-title>
+              <v-card-text>・専攻科棟3F・4F・5Fはクラス教室しかないため<br>表示していない。<v-spacer></v-spacer><br>・スマホ向けのWebアプリである。<v-spacer></v-spacer><br>・設置していない場所は施錠表示。
+
+              </v-card-text>
+            </v-card>
+        </v-col>
+
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+import axios from 'axios';
+ 
+export default {
+ 
+  data() {
+    return {
+      items:null,
+    }
+  },
+ 
+  async mounted() {
+    await this.getData();
+  },
+ 
+  methods:{
+    getData: async function(){
+      const result = await axios(
+          {
+          method:'GET',// GET,POSTなど
+          url:'https://b2q74etkx0.execute-api.us-east-2.amazonaws.com/slfuncapi/dynamodbctrl',// APIのURL
+          headers:{
+            'X-Api-Key':'hx8xubEGea1RYMPawUvfC5pfMgNUefRaaUgVPoym'
+          }
+          },
+          ).then(response => this.items = response.data);
+    }
+  },
+  
+}
+</script>
---
+++ pages/map.vue
@@ -0,0 +1,172 @@
+<template>
+  <v-card 
+    max-width="400"
+    class="mx-auto">
+    <v-container fluid>   
+      <v-row dense>
+        <v-col cols="12">
+          <v-card
+            color="#406882"
+            dark>
+            <v-card-title class="text-h5">
+              校舎外施設
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-card-subtitle></v-card-subtitle>
+            <v-card-text></v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card 
+          color="#E4F9F5">
+               <v-img :src="require('@/assets/ichinoseki-kosen.jpg')"
+               height="auto"
+               width="auto">
+               </v-img>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card
+            color="#406882"
+            dark>
+            <v-card-title class="text-h5">
+              管理・教育棟1F
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-card-subtitle></v-card-subtitle>
+            <v-card-text></v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card 
+          color="#E4F9F5">
+               <v-img :src="require('@/assets/1f.jpg')"
+               height="auto"
+               width="auto">
+               </v-img>
+          </v-card>
+        </v-col>
+
+          <v-col cols="12">
+          <v-card
+            color="#406882"
+            dark>
+            <v-card-title class="text-h5">
+              管理・教育棟2F
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-card-subtitle></v-card-subtitle>
+            <v-card-text></v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card 
+          color="#E4F9F5">
+               <v-img :src="require('@/assets/2f.jpg')"
+               height="auto"
+               width="auto">
+               </v-img>
+          </v-card>
+        </v-col>
+
+          <v-col cols="12">
+          <v-card
+            color="#406882"
+            dark>
+            <v-card-title class="text-h5">
+              管理・教育棟3F
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-card-subtitle></v-card-subtitle>
+            <v-card-text></v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card 
+          color="#E4F9F5">
+               <v-img :src="require('@/assets/3f.jpg')"
+               height="auto"
+               width="auto">
+               </v-img>
+          </v-card>
+        </v-col>
+
+          <v-col cols="12">
+          <v-card
+            color="#406882"
+            dark>
+            <v-card-title class="text-h5">
+              管理・教育棟4F
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-card-subtitle></v-card-subtitle>
+            <v-card-text></v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card 
+          color="#E4F9F5">
+               <v-img :src="require('@/assets/4f.jpg')"
+               height="auto"
+               width="auto">
+               </v-img>
+          </v-card>
+        </v-col>
+
+          <v-col cols="12">
+          <v-card
+            color="#406882"
+            dark>
+            <v-card-title class="text-h5">
+              専攻科棟１F 
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-card-subtitle></v-card-subtitle>
+            <v-card-text></v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card 
+          color="#E4F9F5">
+               <v-img :src="require('@/assets/senkouka1f.jpg')"
+               height="auto"
+               width="auto">
+               </v-img>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card
+            color="#406882"
+            dark>
+            <v-card-title class="text-h5">
+              専攻科棟2F 
+            </v-card-title>
+            <v-spacer></v-spacer>
+            <v-card-subtitle></v-card-subtitle>
+            <v-card-text></v-card-text>
+          </v-card>
+        </v-col>
+
+        <v-col cols="12">
+          <v-card 
+          color="#E4F9F5">
+               <v-img :src="require('@/assets/senkouka2f.jpg')"
+               height="auto"
+               width="auto">
+               </v-img>
+          </v-card>
+        </v-col>
+
+     </v-row>
+    </v-container>
+  </v-card>
+</template>
+
